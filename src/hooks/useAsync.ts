01750806606b80d25b94b00cb942bf41a993bd07
Vinita Jain <jayam6297@gmail.com>
2026-07-13 10:46:30 +0530

    Added useAsync custom hook

---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,23 @@
+import { useCallback, useEffect, useState, type SetStateAction } from 'react';
+
+export default function useAsync(callback: () => Promise<SetStateAction<undefined>>, dependencies: any[] = []) {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState();
+	const [value, setValue] = useState();
+
+	const callbackMemoized = useCallback(() => {
+		setLoading(true);
+		setError(undefined);
+		setValue(undefined);
+		callback()
+			.then(setValue)
+			.catch(setError)
+			.finally(() => setLoading(false));
+	}, [callback, ...dependencies]);
+
+	useEffect(() => {
+		callbackMemoized();
+	}, [callbackMemoized]);
+
+	return { loading, error, value };
+}
